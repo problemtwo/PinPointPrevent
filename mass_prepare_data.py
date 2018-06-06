f63fd661f6a4c2a8f361365cb12f9e247a07d14b
the-inhuman-account <main.ditto@gmail.com>
2018-06-05 18:55:21 -0700

    added mass_prepare_data.py

--- mass_prepare_data.py
+++ mass_prepare_data.py
@@ -1,4 +1,10 @@
+# mass_prepare_data.py
+# This python program takes an input directory and in output directory (in that order) from the command line, and crops out faces in subdirectories.
+
 import os
 from prepare_data import main
 
 for d in os.listdir(sys.argv[1]):
+	if not os.path.exists(os.path.join('training',d)):
+		os.makedir(os.path.join('training',d))
+	main(os.path.join(sys.argv[1],d),d)
